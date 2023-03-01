a949ba0e45580e418c24b95a969be99461ca13ef
fffooofoooo <bigbudbcb@gmail.com>
2023-03-01 14:51:26 -0600

    no message

--- 2022-2023_shared/src/main/java/frc/robot/Robot.java
+++ 2022-2023_shared/src/main/java/frc/robot/Robot.java
@@ -3,7 +3,13 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import javax.swing.text.Position;
 
+import com.ctre.phoenixpro.*;
+import com.ctre.phoenixpro.hardware.CANcoder;
+import com.ctre.phoenixpro.hardware.TalonFX;
+import com.ctre.phoenixpro.controls.*;
+import com.ctre.phoenixpro.configs.*;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -11,11 +17,24 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
+  private final TalonFX john13 = new TalonFX(13,"canivore");
+  private final CANcoder john12 = new CANcoder(12,"canivore");
+  private final CANcoder john22 = new CANcoder(22,"canivore");
+  private final CANcoder john32 = new CANcoder(32,"canivore");
+  private final CANcoder john42 = new CANcoder(42,"canivore");
+  private final DutyCycleOut regularturn =  new DutyCycleOut(0.2);
+
   private RobotContainer m_robotContainer;
 
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    System.out.println("hiamoghus");
+    System.out.println("amongus\n\n\n\n");
+
+    CANcoderConfiguration canconfig = new CANcoderConfiguration();
+    canconfig.MagnetSensor.MagnetOffset = 0;
+    john12.getConfigurator().apply(canconfig);
   }
 
   @Override
@@ -50,12 +69,13 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
     }
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    System.out.println(john12.getPosition() + " | " + john22.getPosition() + " | " + john32.getPosition() + " | " + john42.getPosition() + "\n");
+  }
 
   @Override
   public void teleopExit() {}
